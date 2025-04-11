1acaa67bd117df4346df686e0fe2572630565963
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-04-11 15:41:08 +0000

    Volume of a Triangular Pyramid

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kukwac
+// Created on: March 2022
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the volume of a triangular pyramid.
+ */
+function calculate () {
+  // input
+    let baseA = parseFloat(document.getElementById('baseA').value);
+    let baseB = parseFloat(document.getElementById('baseB').value);
+    let height = parseFloat(document.getElementById('height').value);
+    
+  // process
+    let volume = (1/3) * baseA * baseB * height;
+
+  // output
+    document.getElementById('volume').innerHTML = 'The volume is: ' + volume.toFixed(2) + ' cm³' + "."
+}
